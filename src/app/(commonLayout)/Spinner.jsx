b50fbfb6571b32dcda5ee6a1ecda9cb68b8dae27
MdJowelAhmed <mdjowelahmed924@gmail.com>
2025-05-29 20:14:30 +0600

    comment function added

--- src/app/(commonLayout)/Spinner.jsx
+++ src/app/(commonLayout)/Spinner.jsx
@@ -1,7 +1,7 @@
 import "./spinner.css";
 const Spinner = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-96">
       <div className="loader"></div>
     </div>
   );
