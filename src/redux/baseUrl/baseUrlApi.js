d9794de36dd332ffa9f61713c81f99f611c07b86
MdJowelAhmed <mdjowelahmed924@gmail.com>
2025-07-26 17:41:11 +0600

    update forgoten password functionality

--- src/redux/baseUrl/baseUrlApi.js
+++ src/redux/baseUrl/baseUrlApi.js
@@ -1,2 +1,2 @@
-export const baseUrlApi = "http://10.10.7.37:7000"
-// export const baseUrlApi = "https://api.yogawithjen.life"
+// export const baseUrlApi = "http://10.10.7.37:7000"
+export const baseUrlApi = "https://api.yogawithjen.life"
