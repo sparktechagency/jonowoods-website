99d66ba0bb88be6872ecdd9bab5b7dc3818de9fc
Md Jowel Ahmed <155435567+MdJowelAhmed@users.noreply.github.com>
2025-11-15 19:26:07 +0600

    add inispact open this time video lock

--- src/redux/baseUrl/baseUrlApi.js
+++ src/redux/baseUrl/baseUrlApi.js
@@ -1,2 +1,2 @@
-export const baseUrlApi = "http://10.10.7.62:7000/api/v1"
-// export const baseUrlApi = "https://api.yogawithjen.life/api/v1"
+// export const baseUrlApi = "http://10.10.7.62:7000/api/v1"
+export const baseUrlApi = "https://api.yogawithjen.life/api/v1"
